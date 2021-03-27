d705a496c9eb8a6c15fa0a88eb4a6d1a3a713f9f
MSmyth97 <mathew.smyth@outlook.com>
2021-03-27 00:33:19 +0000

    Finish styling and add style linter and readme

---
+++ readme.md
@@ -0,0 +1,19 @@
+```
+npm start
+```
+
+To run the app in dev mode.
+Will run at http://localhost:8080/
+
+```
+npm create
+
+Will build app to the /dist folder.
+```
+
+```
+npm run style-lint
+```
+
+Will run linter for scss files. Currently set up with basic stylelint sass config
+plus sass property order guidelines.
--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo-r.svg';
 
 export const LoginModal = () => {
     return (
-        <div id="box">
+        <div className="box">
             <div className="logo-container">
                 <img id="logo" className="logo" src={logo} alt="Green Energy Logo"/>
             </div>
@@ -14,9 +14,9 @@ export const LoginModal = () => {
             <div className='form-container'>
                 <p className='form-title'>Email Address</p>
                 <form id="log-in-form" className="log-in-form">
-                    <input type="text" maxLength="50" id="email-input"/><br />
+                    <input type="text" maxLength="50" id="email-input" className="email-input"/><br />
                     <input type="checkbox" className="checkbox"/>
-                    <label for="checkbox">Remember This Device</label>
+                    <label for="checkbox">Remember this device</label>
                 </form>
             </div>
             <button className="sign-in-button">Sign In</button>
